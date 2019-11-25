83b99250e90bcf45ace50634b46d96c92f4b399b
Angelo Brandão <angelobms@gmail.com>
2019-11-25 12:39:05 -0300

    added separation actions.

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,14 +1,31 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
 
 // generation -> mesma coisa que async function
 function* addToCart({ id }) {
-  const respose = yield call(api.get, `/products/${id}`);
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
 
-  yield put(addToCartSuccess(respose.data, respose.data));
+  if (productExists) {
+    const amount = productExists.amount + 1;
+
+    yield put(updateAmount(id, amount));
+  } else {
+    const respose = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...respose.data,
+      amount: 1,
+      priceFormatted: formatPrice(respose.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
